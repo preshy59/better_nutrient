36154aeb560e0ecad9c79ca272585f6663696411
Sophie Senge <sophieperks@gmail.com>
2023-02-07 20:10:28 +0000

    several data points in JS

--- script.js
+++ script.js
@@ -21,3 +21,46 @@
 //     });
 //   });
   
+
+let queryURL = "https://api.edamam.com/search?app_id=47e5c8c0&app_key=845a46deb9705db9fc09c18b79695505&q=cake"
+fetch(queryURL)
+	.then(response => response.json())
+	.then(data => {
+    
+    console.log(data)
+    // title
+  console.log(data.hits[0].recipe.label)
+  // image
+  console.log(data.hits[0].recipe.image)
+  // ingredients
+  console.log(data.hits[0].recipe.ingredients[0].text)
+
+  })
+
+
+
+  //function to display recipe cards
+  function recipeCards(data){
+    //clear innerhtml el
+    //data I want to display in cards
+    let recipeTitle = data.hits[0].recipe.label
+    console.log(data.hits[0].recipe.label)
+    //image
+
+    //ingredient lines
+
+    //link
+
+
+  }
+
+
+  //  function to grab data from api edamam
+  function fetchRecipe(foodItem){
+    fetch(`https://api.edamam.com/search?app_id=47e5c8c0&app_key=845a46deb9705db9fc09c18b79695505&q=${foodItem}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      recipeCards(data)
+    })
+  }
